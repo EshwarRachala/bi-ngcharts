ce42b886f4667658005410333d1ed2d9f5d4dc19
eshwar rachala <eshwar.appdev@gmail.com>
2017-04-04 10:49:42 -0400

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+import { SampleModule } from 'bluebi-charts';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +14,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    SampleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
